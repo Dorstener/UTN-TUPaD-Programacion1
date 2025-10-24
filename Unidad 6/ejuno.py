230cfb4fb769d3c2f06391eaaafc38e0a44fdf73
Nicolas Arbeletche <115483792+Dorstener@users.noreply.github.com>
2025-10-24 14:20:46 +0200

    Create ejuno.py

---
+++ Unidad 6/ejuno.py
@@ -0,0 +1,12 @@
+# Crear una función llamada imprimir_hola_mundo que imprima por 
+# pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+# programa principal.
+
+# Definicion de funcion
+
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+
+# programa principal
+
+imprimir_hola_mundo()
