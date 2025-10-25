2a7620406e49f6f344efc0847177e4f7475db252
Nicolas Arbeletche <115483792+Dorstener@users.noreply.github.com>
2025-10-25 12:43:58 +0200

    ej 2 y 3
    Resolucion ej dos

---
+++ Unidad 7/ejdos.py
@@ -0,0 +1,13 @@
+# Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
+# ● Banana = 1330
+# ● Manzana = 1700
+# ● Melón = 2800
+
+
+precio_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melon': 3000, 'Uva': 1450, 'Naranja': 1200, 'Manzana': 1500, 'Pera': 2300}
+
+precio_frutas['Banana'] = 1330
+precio_frutas['Manzana'] = 1700
+precio_frutas['Melon'] = 2800
+
+print(precio_frutas)
