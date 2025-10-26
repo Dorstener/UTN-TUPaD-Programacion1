f82a77ff3b106e3d21fc27b93a9e59703c830ec8
Nicolas Arbeletche <115483792+Dorstener@users.noreply.github.com>
2025-10-27 00:46:04 +0100

    ejuno

---
+++ Unidad 8/ejuno.py
@@ -0,0 +1,8 @@
+# Crear archivo inicial con productos: Crear un archivo de texto llamado
+# productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantidad
+
+with open("productos.txt","w") as archivo:
+    archivo.write("Nombre,Precio,Cantidad\n")
+    archivo.write("Pelota,80,50\n")
+    archivo.write("Camiseta de Futbol,120,250\n")
+    archivo.write("Guantes de Arquero,10,30\n")
