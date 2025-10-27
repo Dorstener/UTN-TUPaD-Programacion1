6f4db978bfebadc4661db7766e0a149e7ac5fdac
Nicolas Arbeletche <115483792+Dorstener@users.noreply.github.com>
2025-10-27 01:12:16 +0100

    ejdos

---
+++ Unidad 8/ejdos.py
@@ -0,0 +1,8 @@
+# Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada
+# línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente
+# formato:
+
+with open("productos.txt","r") as archivo:
+    for linea in archivo:
+        nombre, precio, cantidad = linea.strip().split(",")
+        print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
--- Unidad 8/ejuno.py
+++ Unidad 8/ejuno.py
@@ -2,7 +2,6 @@
 # productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantidad
 
 with open("productos.txt","w") as archivo:
-    archivo.write("Nombre,Precio,Cantidad\n")
     archivo.write("Pelota,80,50\n")
     archivo.write("Camiseta de Futbol,120,250\n")
     archivo.write("Guantes de Arquero,10,30\n")
